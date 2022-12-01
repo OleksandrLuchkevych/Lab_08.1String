bbe8d9bb939a43ef1eeb23de25d988750771e86d
oleksandr.luchkevych.ri.2022@lpnu.ua <oleksandr.luchkevych.ri.2022@lpnu.ua>
2022-12-01 23:27:29 +0200

    Add project files.

---
+++ Lab_08.1String/Lab_08.1String.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int Count(const string s)
+{
+	int k = 0;
+	size_t pos = 0;
+	while ((pos = s.find('.', pos)) != -1)
+	{
+		pos++;
+		k++;
+		if (k == 3)
+			return pos;
+			
+	}
+	return 0;
+}
+string Change(string& s)
+{
+	int i = 4;
+	int len = s.length();
+	size_t pos = 3;
+
+	while ((pos = s.find(s[pos], pos)) < (len / 4) * 4 - 1)
+	{
+		s.replace(pos, 1, ".");
+		pos += 4;
+	}
+			s.replace(pos, 1, ".");
+			return s;
+}
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	cout << "Index of 3rd dot: " << Count(str) << endl;
+	string dest = Change(str);
+	cout << "Modified string (param) : " << str << endl;
+	cout << "Modified string (result): " << dest << endl;
+		return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <string>
+#include <iostream>
+#include "../Lab_08.1String/Lab_08.1String.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			string str{ "hi...kl" };
+			int t = Count(str);
+			Assert::AreEqual(t, 5);
+		}
+	};
+}
